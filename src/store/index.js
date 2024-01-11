7236011575a3b82d2a82389cd21504d6b1d8f898
tlcgeyer <tamlincupido2402@gmail.com>
2024-01-11 16:30:27 +0200

    add

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,51 @@
 import { createStore } from 'vuex'
+const dataUrl = 'https://tlcgeyer.github.io/vue.portfolioData-tlg/data/'
 
 export default createStore({
-  state: {
+  state: { //allow us to access any variable
+    skills: null ,
+    projects: null ,
+    testimonials: null,
   },
   getters: {
   },
-  mutations: {
+  mutations: { //allow us to update the state
+    setSkills(state, value){
+      state.skills = value
+    },
+    
+    setProjects(state, value){
+      state.projects = value
+    },
+    
+    setTestimonials(state, value){
+      state.testimonials = value
+    },
+    
   },
-  actions: {
+  actions: { //allows us to fetch de data
+
+    async fetchEducation(context) {
+      let res = await fetch(dataUrl)
+      let {education} = await res.json()
+      context.commit('setEducation', education)
+    },
+    async fetchSkills(context){ //need to make use of await each time u make use of async 
+      let res = await fetch(dataUrl) //the destructing
+      let {skills} = await res.json()    //targeting this specific data & grabbing the data from the json
+      context.commit('setSkills', skills) //you change something and need to save it permanently
+      
+    },
+    async fetchProjects() {
+      let res = await fetch(dataUrl)
+      let {projects} = await res.json()
+      context.commit('setProjects',projects)
+    },
+    async fetchTestimonials() {
+      let res = await fetch(dataUrl)
+      let {testimonials} = await res.json()
+      context.commit('setTestimonials',testimonials)
+    }
   },
   modules: {
   }
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,13 +1,36 @@
-<template>
-    <div class="resume">
-      <h1>This is my resume</h1>
+<template> 
+    
+    <!--Education cards-->
+ 
+   <!-- <div class="education" v-if="$store.state.education">
     </div>
+    <div class="skills" v-if="$store.state.skills">
+    </div> -->
+<!-- <div class="row" v-for=" thing in skills" :key="skills.id"> to uniquely identify something unique
+     <p class="lead" >{{ thing.id }}</p> 
+    </div>  -->
+
+    <!--Skills cards : they gonna flip and display the information behind them,add a hover over it-->
+
   </template>
+  <!-- for the img :src="school.image" -->
   
   
   
   <script>
       export default {
-          name: 'ResumeView'
+          name: 'ResumeView',
+          computed: {
+            education() {
+              return this.$store.state.education
+            },
+            skills() {
+              return this.$store.state.skills
+            }
+          },
+          mounted() {
+            this.$store.dispatch('fetchEducation')
+            this.$store.dispatch('fetchSkills')
+          }
       }
   </script>
