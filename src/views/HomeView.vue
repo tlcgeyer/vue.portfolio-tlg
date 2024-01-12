dc0d39966f1d70b5e79103a200fc5e2be660958a
tlcgeyer <tamlincupido2402@gmail.com>
2024-01-12 11:09:00 +0200

    fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@
 
                     <div class="ellipses-container-fluid">
 
-                        <h2 class="greeting">Welcome<br> to my <em>Portfolio</em></h2>
+                        <h2 class="greeting" id="text-animation"><em>Aspiring <br> <br> Full-stack <br><br>Developer</em></h2>
 
                         <div class="ellipses ellipses__outer--thin">
 
@@ -44,62 +44,12 @@ export default {
   components: {
     
   },
-  // mounted() {
-  //   var colors = [
-  //     [251, 243, 246],
-  //     [200, 220, 208],
-  //     [111, 123, 140],
-  //     [244, 226, 156],
-  //     [141, 158, 170]
-  //   ];
-
-  //   var step = 0;
-  //   var colorIndices = [0, 1, 2, 3];
-  //   var gradientSpeed = 0.015;
-
-  //   function updateGradient() {
-  //     var c0_0 = colors[colorIndices[0]];
-  //     var c0_1 = colors[colorIndices[1]];
-  //     var c1_0 = colors[colorIndices[2]];
-  //     var c1_1 = colors[colorIndices[3]];
-
-  //     var istep = 1 - step;
-  //     var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-  //     var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-  //     var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-  //     var color1 = "#" + ((r1 << 16) | (g1 << 8) | b1).toString(16);
-
-  //     var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-  //     var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-  //     var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-  //     var color2 = "#" + ((r2 << 16) | (g2 << 8) | b2).toString(16);
-
-  //     document.getElementById('gradient').style.background = `-webkit-radial-gradient(center, circle cover, ${color1},${color2})`;
-
-  //     step += gradientSpeed;
-  //     if (step >= 1) {
-  //       step %= 1;
-  //       colorIndices[0] = colorIndices[1];
-  //       colorIndices[2] = colorIndices[3];
-
-  //       colorIndices[1] = (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
-  //       colorIndices[3] = (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
-  //     }
-  //   }
-
-  //   setInterval(updateGradient, 10);
-
-  //   document.body.style.margin = '0';
-  //   document.body.style.height = '100vh';
-  //   document.documentElement.style.height = '100vh';
-  // },
 };
 
 
 </script>
 
 <style scoped>
-az
 
 /* Color variables */
 :root {
@@ -156,9 +106,21 @@ h1 {
   text-align: center;
 }
 
+#text-animation {
+  animation: fadeInOut 2s infinite;
+}
+
+@keyframes fadeInOut {
+  0%, 100% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+}
 .coords {
   font-size: 1rem;
-  color: darkblue ;  
+  color: white;  
   display: inline-block;
   transform: rotate(-90deg) translateY(50%);
   float: left;
@@ -190,7 +152,7 @@ h1 {
   font-size: 2.2rem;
   font-weight: 400;
   opacity: 0.5;
-  color: #0C383E;
+  color: whitesmoke;
 }
 
 .greeting:after {
